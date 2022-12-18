a11366aa2a545c3e3b78d3a8d54030c64b47b14a
dmytro-bieliei <dmbieliei@gmail.com>
2022-12-18 20:20:23 +0200

    fix mistakes in userList

--- src/usersList/userCard/index.jsx
+++ src/usersList/userCard/index.jsx
@@ -4,15 +4,19 @@ class UserCard extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      test: 123
+    };
   }
   render() {
-    const { id, firstName, lastName } = this.props;
-    const {} = this.state;
+    const { id, name, lastName } = this.props;
+    const { test } = this.state;
 
     return (
       <div>
-        <p>{firstName}</p>
+        <p>id: {id}</p>
+        <p>{name}</p>
+        <p>{lastName}</p>
       </div>
     );
   }
